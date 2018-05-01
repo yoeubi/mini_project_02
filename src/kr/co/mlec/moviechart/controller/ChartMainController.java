651410e627c809240b07f7a160a650173a8d1994
LAPTOP-L2DDEPJC\eunj2n <eunj525@naver.com>
2018-05-01 11:02:36 +0900

    xml 수정

--- src/kr/co/mlec/moviechart/controller/ChartMainController.java
+++ src/kr/co/mlec/moviechart/controller/ChartMainController.java
@@ -1,6 +1,7 @@
 package kr.co.mlec.moviechart.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,13 +10,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.co.mlec.common.db.MyAppSqlConfig;
+import kr.co.mlec.repository.domain.MovieInfo;
+import kr.co.mlec.repository.mapper.MovieInfoMapper;
+
 @WebServlet("/moviechart/main")
 public class ChartMainController extends HttpServlet{
 	@Override
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		MovieInfoMapper mapper = MyAppSqlConfig.getSqlSession().getMapper(MovieInfoMapper.class);
-//		List<MovieInfo> chart = mapper.selectMovieInfo();
-//		request.setAttribute("chart", chart);
+		MovieInfoMapper mapper = MyAppSqlConfig.getSqlSession().getMapper(MovieInfoMapper.class);
+		List<MovieInfo> chart = mapper.selectMovieInfo();
+		request.setAttribute("chart", chart);
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/jsp/moviechart/main.jsp");
 		rd.forward(request, response);
